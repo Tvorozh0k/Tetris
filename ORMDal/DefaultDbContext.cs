46f6ddb0941e51605ff0ee4890df677d9ebcb92e
Oleg Moskvitin <olegmoskvitinsgu@gmail.com>
2022-12-09 00:35:32 +0400

    Добавьте файлы проекта.

---
+++ ORMDal/DefaultDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ORMDal
+{
+    public partial class DefaultDbContext : DbContext
+    {
+        public DefaultDbContext()
+        {
+        }
+
+        public DefaultDbContext(DbContextOptions<DefaultDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Games> Games { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Tetris;Integrated security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Games>(entity =>
+            {
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Games)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Games_UserId");
+            });
+
+            modelBuilder.Entity<Orders>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Tetris/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Tetris.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tetris.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index(int id, string name)
+        {
+            TempData["Success"] = "Success";
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Json(new { Id = 1, Name = "Andrey" });
+        }
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
